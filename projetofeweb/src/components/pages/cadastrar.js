b6a4cc8f11721e3390b1e5525e5b38b4a479317f
mariaclarabarbosa <clara_estrella@protonmail.com>
2020-09-21 12:53:47 -0300

    Modificação do nome das variáveis

--- projetofeweb/src/components/pages/cadastrar.js
+++ projetofeweb/src/components/pages/cadastrar.js
@@ -7,86 +7,87 @@ import api from '../../services/api'
 
 export default function Cadastrar() {
     const [cliente, setCliente] = useState({})
-    const [vnome, setVnome] = useState('')
-    const [vusuario, setVusuario] = useState('')
-    const [vcpf, setVcpf] = useState('')
-    const [vemail, setVemail] = useState('')
-    const [vnascimento, setVnascimento] = useState('')
-    const [vrua, setVrua] = useState('')
-    const [vnumero, setVnumero] = useState('')
-    const [vcomplemento, setVcomplemento] = useState('')
-    const [vbairro, setVbairro] = useState('')
-    const [vcidade, setVcidade] = useState('')
-    const [vestado, setVestado] = useState('')
-    const [vcep, setVcep] = useState('')
+    const [name, setName] = useState('')
+    const [user, setUser] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [mail, setMail] = useState('')
+    const [birthday, setBirthday] = useState('')
+    const [street, setStreet] = useState('')
+    const [number, setNumber] = useState('')
+    const [complement, setComplement] = useState('')
+    const [district, setDistrict] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [zoneCode, setZoneCode] = useState('')
 
 
-    const handleNome = (e) => {
-        setVnome(e.target.value);
+    const getName = (e) => {
+        setName(e.target.value);
     }
-    const handleUsuario = (e) => {
-        setVusuario(e.target.value);
+    const getUser = (e) => {
+        setUser(e.target.value);
     }
-    const handleCpf = (e) => {
-        setVcpf(e.target.value);
+    const getCpf = (e) => {
+        setCpf(e.target.value);
     }
-    const handleEmail = (e) => {
-        setVemail(e.target.value);
+    const getMail = (e) => {
+        setMail(e.target.value);
     }
-    const handleNascimento = (e) => {
-        setVnascimento(e.target.value);
+    const getBirthday = (e) => {
+        setBirthday(e.target.value);
     }
-    const handleRua = (e) => {
-        setVrua(e.target.value);
+    const getStreet = (e) => {
+        setStreet(e.target.value);
     }
-    const handleNumero = (e) => {
-        setVnumero(e.target.value);
+    const getNumber = (e) => {
+        setNumber(e.target.value);
     }
-    const handleComplemento = (e) => {
-        setVcomplemento(e.target.value);
+    const getComplement = (e) => {
+        setComplement(e.target.value);
     }
-    const handleBairro = (e) => {
-        setVbairro(e.target.value);
+    const getDistrict = (e) => {
+        setDistrict(e.target.value);
     }
-    const handleCidade = (e) => {
-        setVcidade(e.target.value);
+    const getCity = (e) => {
+        setCity(e.target.value);
     }
-    const handleEstado = (e) => {
-        setVestado(e.target.value);
+    const getState = (e) => {
+        setState(e.target.value);
     }
-    const handleCep = (e) => {
-        setVcep(e.target.value);
+    const getZoneCode = (e) => {
+        setZoneCode(e.target.value);
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        var cliente = {
-            nome: vnome,
-            usuario: vusuario,
-            cpf: vcpf,
-            email: vemail,
-            dataNascimento: vnascimento,
+        var customer = {
+            nome: name,
+            usuario: user,
+            cpf: cpf,
+            email: mail,
+            dataNascimento: birthday,
             endereco: {
-                rua: vrua,
-                numero: vnumero,
-                complemento: vcomplemento,
-                bairro: vbairro,
-                cidade: vcidade,
-                estado: vestado,
-                cep: vcep
+                rua: street,
+                numero: number,
+                complemento: complement,
+                bairro: district,
+                cidade: city,
+                estado: state,
+                cep: zoneCode
             }
         }
-        setCliente(cliente)
+        setCliente(customer)
         console.log(cliente)
-        // api.post('/cliente', cliente).then(res => console.log(res)).catch(erro => console.log(erro))
+        console.log(customer)
+        api.post('/cliente', cliente).then(res => console.log(res)).catch(erro => console.log(erro))
     }
 
-    useEffect(() => {
-        api.post('/cliente', cliente).then(response => {
-            console.log(response.data)
-        }).catch(erro => console.log(erro))
-    }, [cliente])
+    // useEffect(() => {
+    //     api.post('/cliente', cliente).then(response => {
+    //         console.log(response.data)
+    //     }).catch(erro => console.log(erro))
+    // }, [cliente])
 
     return (
 
@@ -101,55 +102,55 @@ export default function Cadastrar() {
                             <fieldset>
                                 <div className="form-group">
                                     <label htmlFor="nome">Nome do cliente</label>
-                                    <input type="text" required={true} className="nome form-control" id="nome" autoFocus onChange={handleNome} />
+                                    <input type="text" required={true} className="nome form-control" id="nome" autoFocus onChange={getName} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="usuario">Usuário</label>
-                                    <input type="text" required={true} className="usuario form-control" id="usuario" onChange={handleUsuario} />
+                                    <input type="text" required={true} className="usuario form-control" id="usuario" onChange={getUser} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="cpf">CPF</label>
-                                    <input type="text" required={true} placeholder="000.000.000.00" className="cpf form-control" id="cpf" onChange={handleCpf} />
+                                    <input type="text" required={true} placeholder="000.000.000.00" className="cpf form-control" id="cpf" onChange={getCpf} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="email">E-mail</label>
                                     <div className="input-group-prepend">
                                         <span className="input-group-text">@</span>
-                                        <input type="email" required={true} placeholder="email@example.com" className="emai form-control" id="email" onChange={handleEmail} />
+                                        <input type="email" required={true} placeholder="email@example.com" className="emai form-control" id="email" onChange={getMail} />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="dataNascimento">Data de nascimento</label>
-                                    <input type="text" required={true} placeholder="AAAA-MM-DD" className="dataNascimento form-control" id="dataNascimento" onChange={handleNascimento} />
+                                    <input type="text" required={true} placeholder="AAAA-MM-DD" className="dataNascimento form-control" id="dataNascimento" onChange={getBirthday} />
                                 </div>
                                 <h3>Endereço</h3>
                                 <div className="form-group">
                                     <label htmlFor="rua">Rua</label>
-                                    <input type="text" required={true} className="rua form-control" id="rua" onChange={handleRua} />
+                                    <input type="text" required={true} className="rua form-control" id="rua" onChange={getStreet} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="numero">Número</label>
-                                    <input type="text" required={true} className="numero form-control" id="numero" onChange={handleNumero} />
+                                    <input type="text" required={true} className="numero form-control" id="numero" onChange={getNumber} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="complemento">Complemento</label>
-                                    <input type="text" className="complemento form-control" id="complemento" onChange={handleComplemento} />
+                                    <input type="text" className="complemento form-control" id="complemento" onChange={getComplement} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="bairro">Bairro</label>
-                                    <input type="text" required={true} className="bairro form-control" id="bairro" onChange={handleBairro} />
+                                    <input type="text" required={true} className="bairro form-control" id="bairro" onChange={getDistrict} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="cidade">Cidade</label>
-                                    <input type="text" required={true} className="cidade form-control" id="cidade" onChange={handleCidade} />
+                                    <input type="text" required={true} className="cidade form-control" id="cidade" onChange={getCity} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="estado">Estado</label>
-                                    <input type="text" required={true} className="estado form-control" id="estado" onChange={handleEstado} />
+                                    <input type="text" required={true} className="estado form-control" id="estado" onChange={getState} />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="cep">Cep</label>
-                                    <input type="text" required={true} className="cep form-control" id="cep" onChange={handleCep} />
+                                    <input type="text" required={true} className="cep form-control" id="cep" onChange={getZoneCode} />
                                 </div>
                             </fieldset>
                         </form>
